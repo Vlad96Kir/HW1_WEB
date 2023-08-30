912bc1fb502ce30249600bc3b0675cdf216952df
Vlad96Kir <130667144+Vlad96Kir@users.noreply.github.com>
2023-08-30 15:13:48 +0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,320 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+import re
+
+
+class Field:
+    def __init__(self, value=None):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self.validate(new_value)
+        self._value = new_value
+
+    def validate(self, value):
+        pass
+
+    def __str__(self):
+        return str(self._value)
+
+    def __repr__(self):
+        return str(self._value)
+
+
+class Name(Field):
+    def validate(self, value):  # LS -->
+        if not value or not isinstance(value, str):  # LS -->
+            raise ValueError("The name must be a non-empty string.")
+
+
+class Phone(Field):
+    @staticmethod
+    def validate_phone(value: str):
+        correct = ("(", ")", "-", " ")
+        format_value = value
+        for i in correct:
+            format_value = format_value.replace(i, "")
+        if len(format_value) == 13 and format_value.startswith('+380'):
+            return format_value
+        elif len(format_value) == 12 and format_value.startswith('380'):
+            return f"+{format_value}"
+        elif len(format_value) == 11 and value.startswith('80'):
+            return f"+3{format_value}"
+        elif len(format_value) == 10 and format_value.startswith('0'):
+            return f"+38{format_value}"
+        else:
+            print("Invalid phone number. Please enter a new number.")
+            return f'Number {format_value} is not correct'
+
+
+class Birthday(Field):
+    def validate(self, value):
+        if value:
+            try:
+                datetime.strptime(value, "%d.%m.%Y")
+            except ValueError:
+                raise ValueError("Incorrect birthday format. Please use the format DD.MM.YYYY")
+
+    def to_datetime(self):
+        if self._value:
+            return datetime.strptime(self._value, "%d.%m.%Y")
+
+
+class Address(Field):
+    pass
+
+
+class Email(Field):
+    @staticmethod
+    def input_correct_email(value):
+        while len(value) != 0:
+            def analyze_email(email):
+                pattern = r"(^[a-zA-Z0-9_.+-]{2,}@([a-zA-Z0-9-]{2,}\." \
+                          r"[a-zA-Z0-9]+$|[a-zA-Z0-9-]{2,}\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+$))"
+                return re.match(pattern, email) is not None
+
+            if analyze_email(value):
+                return f"Your email '{value}' is correct"
+            else:
+                value = input("Invalid e-mail address. Enter the email, or leave empty: ")
+            return value
+        return value
+
+
+class Note:
+    def __init__(self, text, tags=None):
+        self.text = text
+        self.tags = tags if tags is not None else []
+
+    def add_tag(self, tag=None):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return "Tags: " + ", ".join(self.tags) + "\n" + "Note: " + self.text
+
+    def __eq__(self, other):
+        return self.text == other.text
+
+
+class NoteBook:
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, note):
+        self.notes.append(note)
+
+    def add_tag_to_note(self, word, tag):
+        matching_notes = self.search_notes_by_word(word)
+
+        if not matching_notes:
+            return f"No notes found for the given keyword '{word}'."
+
+        if len(matching_notes) == 1:
+            note = matching_notes[0]
+            note.add_tag(tag)
+            return f"Tag '{tag}' added to note with text '{note.text}'."
+
+        matching_notes_text = [f"{i}. {note.text}" for i, note in enumerate(matching_notes, start=1)]
+        notes_list = "\n".join(matching_notes_text)
+        choice = input(f"Select a note from the list by entering its number:\n{notes_list}\n")
+
+        if choice.isdigit() and 1 <= int(choice) <= len(matching_notes):
+            note = matching_notes[int(choice) - 1]
+            note.add_tag(tag)
+            return f"Tag '{tag}' added to note with text '{note.text}'."
+        else:
+            return "Invalid choice. Tag not added."
+
+    def get_note_by_text(self, text):
+        for note in self.notes:
+            if note.text == text:
+                return note
+        return None
+
+    def delete_note_by_text(self, text):
+        for note in self.notes:
+            if note.text == text:
+                self.notes.remove(note)
+                return f"Note with text '{text}' removed."
+        return f"Note with text '{text}' not found."
+
+    def edit_note(self, old_text, new_text):
+        for note in self.notes:
+            if note.text == old_text:
+                note.text = new_text
+                return f"Note updated from '{old_text}' to '{new_text}'."
+        return f"Note with text '{old_text}' not found."
+
+    def search_notes_by_word(self, word):
+        results = [note for note in self.notes if word.lower() in note.text.lower()]
+        return results
+
+    def search_notes_by_tag(self, tag):
+        results = [note for note in self.notes if tag in note.tags]
+        return results
+
+    def search_notes_by_tags(self, tags):
+        results = [note for note in self.notes if all(tag in note.tags for tag in tags)]
+        return results
+
+    def to_dict(self):
+        notes_data = [{'text': note.text, 'tags': note.tags} for note in self.notes]
+        return {'notes': notes_data}
+
+    @classmethod
+    def from_dict(cls, data):
+        notebook = cls()
+        notes_data = data.get('notes', [])
+        for note_data in notes_data:
+            note = Note(note_data['text'], note_data['tags'])
+            notebook.add_note(note)
+        return notebook
+
+    def get_notes(self):
+        return self.notes
+
+    def __str__(self):
+        return "\n".join(str(note) for note in self.notes)
+
+
+class Record:
+    def __init__(self, name: Name,
+                 phone: Phone = None,
+                 birthday: Birthday = None,
+                 address: Address = None,
+                 email: Email = None):
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone)
+        self.birthday = birthday
+        self.address = address
+        self.email = email
+
+    def add_phone(self, phone=None, birthday=None):
+        if phone and phone.value not in [p.value for p in self.phones]:
+            self.phones.append(phone)
+        if birthday:
+            while not self.is_valid_birthday_format(birthday.value):
+                print("Incorrect birthday format. Please use the format DD.MM.YYYY")
+                birthday = Birthday(input("Enter birthday in format 'DD.MM.YYYY' (or leave empty if not available): "))
+            if not self.birthday:
+                self.birthday = birthday
+            else:
+                self.birthday.value = birthday.value
+
+    def add_birthday(self, birthday: Birthday):
+        self.birthday = birthday
+
+    @staticmethod
+    def is_valid_birthday_format(value):
+        try:
+            datetime.strptime(value, "%d.%m.%Y")
+            return True
+        except ValueError:
+            return False
+
+    def del_phone(self, phone: Phone):
+        for p in self.phones:
+            if p.value == phone.value:
+                self.phones.remove(p)
+                return f"phone {phone} removed from contact {self.name}"
+        return f"{phone} not present in phones of contact {self.name}"
+
+    def edit_phone(self, old_phone, new_phone):
+        for idx, p in enumerate(self.phones):
+            if old_phone == p.value:  # LS -->
+                self.phones[idx].value = new_phone  # LS -->
+                return f"old phone {old_phone} change to {new_phone}"
+            return f"{old_phone} not present in phones of contact {self.name}"
+
+    def days_to_birthday(self):
+        if self.birthday:
+            today = datetime.now()
+            next_birthday = self.birthday.to_datetime().replace(year=today.year)
+            if next_birthday < today:
+                next_birthday = next_birthday.replace(year=today.year + 1)
+            days_remaining = (next_birthday - today).days
+            return days_remaining
+        return None
+
+    def __str__(self):
+        phones_str = ", ".join(str(p) for p in self.phones)
+        return f"Name: {self.name}, Phones: {phones_str}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record: Record):
+        self.data[str(record.name)] = record
+        return f"Contact {record} add success"
+
+    def delete_record(self, name):
+        del self.data[name]
+
+    def edit_record(self, name, new_record):
+        self.data[name] = new_record
+
+    def search_records(self, **kwargs):
+        results = []
+        for record in self.data.values():
+            match = True
+            for key, value in kwargs.items():
+                if key == "name":
+                    if str(record.name).lower() != value.lower():
+                        match = False
+                        break
+                elif key == "phone":
+                    phone_match = False
+                    for phone in record.phones:
+                        if str(phone).lower() == value.lower():
+                            phone_match = True
+                            break
+                    if not phone_match:
+                        match = False
+                        break
+            if match:
+                results.append(record)
+        return results
+
+    def search_by_name(self, name_query):
+        results = []
+        for record in self.data.values():
+            if name_query.lower() in str(record.name).lower():
+                results.append(record)
+        return results
+
+    def search_by_phone(self, phone_query):
+        results = []
+        for record in self.data.values():
+            for phone in record.phones:
+                if phone_query in str(phone):
+                    results.append(record)
+                    break
+        return results
+
+    def save_to_file(self, file_path):
+        with open(file_path, 'wb') as f:
+            pickle.dump(self.data, f)
+
+    def load_from_file(self, file_path):
+        try:
+            with open(file_path, 'rb') as f:
+                self.data = pickle.load(f)
+        except FileNotFoundError:
+            self.data = {}
+
+    def iterator(self, batch_size, page_number):
+        data_values = list(self.data.values())
+        start_idx = page_number * batch_size
+        end_idx = min((page_number + 1) * batch_size, len(data_values))
+        return data_values[start_idx:end_idx]
+
+    def __str__(self) -> str:
+        return "\n".join(str(r) for r in self.data.values())
---
+++ main.py
@@ -0,0 +1,433 @@
+import pickle
+import difflib
+from Address_Book import sort
+import json
+from Address_Book.classes import AddressBook, Name, Phone, Record, Birthday, Address, Email, Note, NoteBook
+
+address_book = AddressBook()
+notebook = NoteBook()
+
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Contact not found."
+        except ValueError:
+            return "Invalid input. Please enter name or phone number."  # LS -->
+        except IndexError:
+            return "Invalid input. Please enter a command."
+        except TypeError:
+            return func()
+
+    return wrapper
+
+
+@input_error
+def hello():
+    return "How can I help you?"
+
+
+@input_error
+def add_contact():
+    while True:
+        name_input = input("Enter the name or type 'cancel' to cancel: ").strip()
+        if name_input.lower() == 'cancel':
+            return "Contact addition canceled."
+
+        if name_input:
+            name = Name(name_input)  # LS -->
+            rec: Record = address_book.get(str(name))
+
+            if rec:
+                return "Contact already exists. Use 'edit phone', 'edit birthday', etc., to modify the contact."
+
+            phone_input = input("Enter the phone number: ").strip()
+            if phone_input:
+                phone_input = Phone.validate_phone(phone_input)
+                while not phone_input:
+                    phone_input = input("Enter the phone number: ").strip()
+                    phone_input = Phone.validate_phone(phone_input)
+
+            birthday_input = input("Enter birthday in format 'DD.MM.YYYY' (or leave empty if not available): ")
+            while birthday_input and not Record.is_valid_birthday_format(birthday_input):
+                print("Incorrect birthday format. Please use the format DD.MM.YYYY.")
+                birthday_input = input("Enter birthday in format 'DD.MM.YYYY' (or leave empty if not available): ")
+
+            address_input = input("Enter the address, or leave empty: ")
+            email_input = Email.input_correct_email(input("Enter the email, or leave empty: "))
+
+            phone = Phone(phone_input) if phone_input else None
+            birthday = Birthday(birthday_input) if birthday_input else None
+            address = Address(address_input) if address_input else None
+            email = Email(email_input) if email_input else None
+
+            rec = Record(name, phone, birthday, address, email)
+            address_book.add_record(rec)
+            return f"Contact '{name}' successfully added."
+        else:
+            print("Name is required. Please enter a name.")
+
+
+def del_phone():
+    name = Name(input("Enter the name: "))
+    phone = Phone(input("Enter the phone number: ").strip())
+    rec: Record = address_book.get(str(name))
+    if rec:
+        rec.del_phone(Phone(phone))  # LS -->
+        return f"The phone number '{phone}' has been removed from the contact '{name}'."
+    return f"No contact '{name}' in address book"
+
+
+@input_error
+def add_phone():
+    name = Name(input("Enter the name: ").strip())
+    rec: Record = address_book.get(str(name))
+    if rec:
+        phone_input = Phone(input("Enter the phone number: ").strip())
+
+        new_phone = Phone(phone_input)
+        rec.add_phone(new_phone)
+        return f"Phone number '{new_phone}' added to contact '{name}'."
+    return f"No contact '{name}' in the address book"
+
+
+@input_error
+def edit_phone():
+    name = Name(input("Enter the name: ").strip())
+    rec: Record = address_book.get(str(name))
+    if rec:
+        phone_input = Phone(input("Enter the phone number: ").strip())
+
+        new_phone = Phone(phone_input)
+        rec.add_phone(new_phone)
+        return f"Phone number '{new_phone}' added to contact '{name}'."
+    return f"No contact '{name}' in address book"
+
+
+@input_error
+def change_phone():
+    name = Name(input("Enter the name: ").strip())
+    old_phone = Phone(input("Enter the old phone number: ").strip())
+    new_phone = Phone(input("Enter the new phone number: ").strip())
+
+    rec: Record = address_book.get(str(name))
+    if rec:
+        rec.edit_phone(old_phone, new_phone)
+        return f"The old phone number '{old_phone}' has been updated to '{new_phone}' for contact '{name}'."
+    return f"No contact '{name}' in address book"
+
+
+@input_error
+def add_birthday():
+    name = Name(input("Enter the name: ").strip())
+    birthday = Birthday(input("Enter birthday in format 'DD.MM.YYYY': "))
+    rec: Record = address_book.get(str(name))
+    if rec:
+        rec.birthday = birthday
+        return f"Birthday updated for contact '{name}'."
+    return f"No contact '{name}' in address book"
+
+
+@input_error
+def days_to_birthday():
+    name = Name(input("Enter the name: ").strip())
+    rec: Record = address_book.get(str(name))
+    if rec and rec.birthday:
+        days_left = rec.days_to_birthday()
+        if days_left is not None:
+            if days_left == 0:
+                return f"Contact '{name}' birthday is today!"
+            elif days_left == 1:
+                return f"Contact '{name}' birthday is tomorrow!"
+            return f"Contact '{name}' birthday is in '{days_left}' days."
+        return f"Contact '{name}' birthday is today!"
+    return f"The contact '{name}' is not found in the address book or the birthday is not specified."
+
+
+@input_error
+def edit_birthday():
+    name = Name(input("Enter the name: ").strip())
+    rec: Record = address_book.get(str(name))
+    if rec:
+        new_birthday = Birthday(input("Enter a new birthday (in DD.MM.YYYY format): "))
+        rec.birthday = new_birthday
+        return f"Birthday updated for contact '{name}'."
+    return f"No contact '{name}' in address book"
+
+
+def show_birthday_within_days():
+    try:
+        days = int(input("Enter the number of days to check: "))
+    except ValueError:
+        return "Invalid input. Please enter a valid number of days."
+
+    birthday_contacts = []
+    for name, record in address_book.data.items():
+        if record.birthday and record.days_to_birthday() == days:
+            birthday_contacts.append(record)
+
+    if birthday_contacts:
+        output = f"Contacts with birthdays {days} days from now:\n\n"
+        for contact in birthday_contacts:
+            contact_info = f"Name: {contact.name};" \
+                           f" Phones: {', '.join(str(phone) for phone in contact.phones)};" \
+                           f" Birthday: {contact.birthday};"
+            output += f"{contact_info}\n"
+        return output
+    else:
+        return f"No contacts have birthdays {days} days from now."
+
+
+@input_error
+def show_all():
+    page_number = 1
+    batch_size = 5
+
+    while True:
+        records_batch = list(address_book.iterator(batch_size, page_number - 1))
+        if not records_batch:
+            print("No more contacts to display.")
+            break
+
+        for record in records_batch:
+            birthday_info = f"birthday: {record.birthday.value};" if record.birthday else ""  # LS -->
+            phones_info = f"phones: {' / '.join(str(phone) for phone in record.phones)};" if record.phones else ""
+            address_info = f"address: {record.address.value};" if record.address else ""  # LS -->
+            email_info = f"email: {record.email.value}." if record.email else ""  # LS -->
+            print(f"Contact: name '{record.name}' {phones_info} {birthday_info} {address_info} {email_info}")  # LS -->
+
+        print("\nPage:", page_number)
+        print("Press Enter to see the next page or type 'exit' to return to the main menu.\n")
+        user_input = input()
+
+        if user_input.lower() == 'exit':
+            break
+        else:
+            page_number += 1
+    return "Continue...\n"
+
+
+def search_by_name():
+    name_query = input("Enter the name or part of the name to search: ").strip()
+    results = address_book.search_by_name(name_query)
+    if results:
+        return "\n".join(str(record) for record in results)
+    return "No contacts found for the given name."
+
+
+def search_by_phone():
+    phone_query = input("Enter the phone or part of the phone to search: ").strip()
+    results = address_book.search_by_phone(phone_query)
+    if results:
+        return "\n".join(str(record) for record in results)
+    return "No contacts found for the given phone."
+
+
+def sort_directory():
+    folder_path = input("Enter the folder path to sort: ")
+    result = sort.sort_folder(folder_path)
+    return result
+
+
+def add_note():
+    text = input("Enter the note text: ")
+    note = Note(text)
+    notebook.add_note(note)
+    return f"Note '{text}' added."
+
+
+@input_error
+def add_tag():
+    while True:
+        note_query = input("Enter a keyword to search for notes: ")
+        matching_notes = notebook.search_notes_by_word(note_query)
+
+        if not matching_notes:
+            print(f"No notes found for the given keyword. Please try again.")
+        else:
+            print("Matching notes:")
+            for i, note in enumerate(matching_notes, start=1):
+                print(f"{i}. {note.text}")
+            choice = input("Enter the number of the note to add a tag to: ")
+
+            if choice.isdigit() and 1 <= int(choice) <= len(matching_notes):
+                selected_note = matching_notes[int(choice) - 1]
+                tag = input("Enter the tag: ")
+                result = notebook.add_tag_to_note(selected_note.text, tag)
+                print(result)
+                break
+            else:
+                print("Invalid choice. Please enter a valid number.")
+
+
+def change_note():
+    text = input("Enter the note text: ")
+    new_text = input("Enter the new note text: ")
+    result = notebook.edit_note(text, new_text)
+    return result
+
+
+def del_note():
+    text = input("Enter the note text: ")
+    result = notebook.delete_note_by_text(text)
+    return result
+
+
+@input_error
+def search_note_by_tag():
+    while True:
+        tag = input("Enter the tag to search for notes or type 'exit' to return to the main menu: ")
+        if tag.lower() == 'exit':
+            return
+        notes_with_tag = notebook.search_notes_by_tag(tag)
+
+        if len(notes_with_tag) == 0:
+            print(f"No notes found with tag '{tag}'. Please try again or type 'exit' to return to the main menu.")
+        else:
+            result = []
+            for note in notes_with_tag:
+                result.append(f"Note: {note.text}")
+            return "\n\n".join(result)
+
+
+@input_error
+def search_note():
+    while True:
+        search_word = input("Enter the word to search in notes or type 'exit' to try searching by tag: ")
+        if search_word.lower() == 'exit':
+            return
+        notes_with_word = notebook.search_notes_by_word(search_word)
+
+        if len(notes_with_word) == 0:
+            print(f"No notes found containing the word '{search_word}'."
+                  f" Please try again or type 'exit' to search by tag.")
+        else:
+            result = []
+            for note in notes_with_word:
+                result.append(f"Note: {note.text}")
+            return "\n\n".join(result)
+
+
+def show_notes():
+    notes = notebook.get_notes()
+    if len(notes) == 0:
+        return "No notes found."
+
+    result = []
+    for note in notes:
+        if len(note.tags) == 0:
+            result.append(f"Note: {note.text}")
+        else:
+            result.append(str(note))
+
+    return "\n\n".join(result)
+
+
+def helper():
+    commands = {
+        hello: "hello -> displays a welcome message.",
+        add_contact: "add -> adds a new contact.",
+        add_birthday: " add birthday -> adds birthday to contact",
+        add_note: "add note -> add a new note",
+        add_tag: "add tag -> add a new tag to a note.",
+        edit_birthday: "edit birthday -> changes the existing birthday value of a contact",
+        days_to_birthday: "days to birthday -> shows how many days are left until the birthday",
+        show_birthday_within_days: "show birthday -> display a list of contacts whose birthday is a specified number"
+                                   " of days from the current date",
+        edit_phone: "edit phone -> changes the phone number of an existing contact.",
+        del_phone: "del phone -> delete number from contact.",
+        del_note: "del note -> delete a note.",
+        add_phone: "add phone -> adds phone to exist contact",
+        change_phone: "change phone -> replaces the old number with a new one",
+        change_note: "change note -> modify an existing note.",
+        show_all: "show all -> displays all contacts and their phone numbers.",
+        show_notes: "show notes -> display all notes.",
+        search_by_name: "search by name -> searches for contacts in which the name coincides",
+        search_by_phone: "search by phone -> looking for contacts with a matching phone number",
+        search_note_by_tag: "search note by tag -> search for a note with a tag.",
+        search_note: "search note -> Search for a note in the text",
+        sort_directory: "sort folder -> sorts files into categories,"
+                        " removes empty folders in the folder path specified by the user",
+        helper: "help -> displays the list of available commands.",
+        exit: "exit, close, good bye -> exits the program."
+    }
+    help_text = "Available commands:\n"
+    for command, description in commands.items():
+        help_text += f"{description}\n"
+    return help_text
+
+
+def find_closest_command(text, commands):
+    available_commands = list(commands.keys())
+    closest_command = difflib.get_close_matches(text.lower(), available_commands, n=1)
+    if closest_command and closest_command[0] != text.lower():
+        return closest_command[0]
+    return None
+
+
+def main():
+    file_path = 'address_book.pkl'
+    file_path_note = 'notebook.txt'
+    try:
+        with open(file_path_note, 'r') as file:
+            notebook_data = json.load(file)
+            notebook = NoteBook.from_dict(notebook_data)
+    except (FileNotFoundError, json.JSONDecodeError):
+        notebook = NoteBook()
+        print("Failed to load the notebook. Starting with an empty notebook.")
+
+    try:
+        address_book.load_from_file(file_path)
+    except pickle.UnpicklingError:
+        print("Failed to load the address book. Starting with an empty address book.")
+
+    print("\nWelcome!\n")
+    commands = {
+        "hello": hello,
+        "add": add_contact,
+        "add note": add_note,
+        "add tag": add_tag,
+        "change note": change_note,
+        "del note": del_note,
+        "search note": search_note,
+        "search note by tag": search_note_by_tag,
+        "show notes": show_notes,
+        "add birthday": add_birthday,
+        "edit birthday": edit_birthday,
+        "show birthday": show_birthday_within_days,
+        "days to birthday": days_to_birthday,
+        "edit phone": edit_phone,
+        "del phone": del_phone,
+        "change phone": change_phone,
+        "show all": show_all,
+        "search by name": search_by_name,
+        "search by phone": search_by_phone,
+        "sort folder": sort_directory,
+        "help": helper,
+        "exit": exit,
+        "good bye": exit,
+        "close": exit
+    }
+
+    while True:
+        command = input("\nEnter a command: ").lower().strip()
+
+        closest_command = find_closest_command(command, commands)
+        if closest_command:
+            print(f"Did you mean '{closest_command}'")
+        else:
+            if command in commands:
+                func = commands[command]
+                print(func())
+            else:
+                print("Invalid command. Please try again.")
+
+        address_book.save_to_file(file_path)
+        with open(file_path_note, 'w') as file:
+            json.dump(notebook.to_dict(), file)
+
+
+if __name__ == "__main__":
+    main()
